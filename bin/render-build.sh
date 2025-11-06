006e48843ee4260ae75d6b2b90521378a23ac9d9
Sentil <sentilkumaran@mallow-tech.com>
2025-11-06 23:20:01 +0530

    Fix Render deployment: disable Docker, use build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Starting Render build..."
+
+echo "Installing gems..."
 bundle install
+
+echo "Precompiling assets..."
 bundle exec rails assets:precompile
+
+echo "Running database migrations..."
 bundle exec rails db:migrate
+
+echo "Build complete!"
 
